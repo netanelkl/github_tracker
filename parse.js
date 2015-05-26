483b9a8bb86b3db43f3c7bb86219c571c3279274
netanel <netanel.keidar@gmail.com>
2015-05-25 20:48:10 -0400

    Some more parse code

--- parse.js
+++ parse.js
@@ -1,21 +1,19 @@
-function parse_setup(onLogin)
-{
+function parse_setup(onLogin) {
 	Parse.initialize(credentials.parse_app_id, credentials.parse_key);
 	var currentUser = Parse.User.current();
 	if (currentUser) {
-	    // do stuff with the user
+		// do stuff with the user
 	} else {
-	    Parse.User.logIn(credentials.email, credentials.password, {
-		success: onLogin,
-		error: function(user, error) {
-			parse_signup(credentials.email, credentials.password, onLogin);
-		}
-});
+		Parse.User.logIn(credentials.email, credentials.password, {
+			success : onLogin,
+			error : function(user, error) {
+				parse_signup(credentials.email, credentials.password, onLogin);
+			}
+		});
 	}
 }
 
-function parse_signup(email, password, onDone)
-{
+function parse_signup(email, password, onDone) {
 	var user = new Parse.User();
 	user.set("username", email);
 	user.set("password", password);
@@ -25,10 +23,10 @@ function parse_signup(email, password, onDone)
 	// user.set("phone", "415-392-0202");
 
 	user.signUp(null, {
-	  success: onDone,
-	  error: function(user, error) {
-	    // Show the error message somewhere and let the user try again.
-	    alert("Error: " + error.code + " " + error.message);
-	  }
+		success : onDone,
+		error : function(user, error) {
+			// Show the error message somewhere and let the user try again.
+			alert("Error: " + error.code + " " + error.message);
+		}
 	});
 }
